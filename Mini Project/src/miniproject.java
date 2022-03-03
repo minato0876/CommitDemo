d68b0141498f6883e3175b71617cbde5edd70ea8
minato0876 <ganupam010@gmail.com>
2022-03-03 09:32:25 +0530

    First Commit

---
+++ Mini Project/src/miniproject.java
@@ -0,0 +1,20 @@
+
+import java.io.*;
+import java.util.*;
+
+public class miniproject {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(new File("D:\\Downloads\\Java Programs\\complaints.csv"));  
+		sc.useDelimiter(",");   //sets the delimiter pattern  
+		while (sc.hasNext())  //returns a boolean value  
+		{  
+		System.out.print(sc.next());  //find and returns the next complete token from this scanner  
+		}   
+		sc.close();  //closes the scanner  
+		
+	}
+
+}
